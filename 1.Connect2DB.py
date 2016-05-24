ac2cc471e94649b70a2c57e014f18eee6acfada2
trinitybest <hutianyou6@gmail.com>
2016-05-25 11:37:31 +1200

    update

--- 1.Connect2DB.py
+++ 1.Connect2DB.py
@@ -16,6 +16,15 @@ def MSSQL_Connect():
 	password = keys['DBpassword']
 	database = 'SeekingAlpha'
 	conn = pymssql.connect(server, user, password, database)
+	cursor = conn.cursor(as_dict= True)
+	cursor.execute("SELECT TOP 1000 * FROM dbo.SeekingAlpha_Articles \
+	WHERE Disclosure != ''")
+	return cursor
 
+def Key_Stats(cursor_para):
+	df = df.DataFrame(column = ['Title', 'Date', 'Time', 'TickersAbout', 'TickersIncludes', 
+			'Name', 'NameLink', 'Bio', 'Summary', 'ImageDummy', 'BodyContent', 'Disclosure', 
+			'Position', 'CreatedAt', 'UpdatedAt', 'BodyAll', 'ArticleNumber', 'ArticleUrl'])
 if __name__ == '__main__':
-	MSSQL_Connect()
+	cur = MSSQL_Connect()
+	
