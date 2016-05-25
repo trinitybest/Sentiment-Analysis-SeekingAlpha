6bb3e57d2e959c775462b1599b20cd84d904eedf
trinitybest <hutianyou6@gmail.com>
2016-05-25 14:54:24 +1200

    update

--- 1.Connect2DB.py
+++ 1.Connect2DB.py
@@ -22,36 +22,51 @@ def MSSQL_Connect():
 	return cursor
 
 def Key_Stats(cursor_para):
-	df = df.DataFrame(column = ['Title', 'Date', 'Time', 'TickersAbout', 'TickersIncludes', 
+	
+	df = pd.DataFrame(columns = ['Title', 'Date', 'Time', 'TickersAbout', 'TickersIncludes', 
 			'Name', 'NameLink', 'Bio', 'Summary', 'ImageDummy', 'BodyContent', 'Disclosure', 
 			'Position', 'CreatedAt', 'UpdatedAt', 'BodyAll', 'ArticleNumber', 'ArticleUrl'])
+
 	row = cursor_para.fetchone()
-	with open("result.csv", "w") as f:
-		f.write("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18}".
-			format(row[''],)) 
-		while row:
-			row = cursor_para.fetchone()
-"""
-	df = df.append({'Title':, 
-					'Date', 
-					'Time', 
-					'TickersAbout', 
-					'TickersIncludes', 
-					'Name', 
-					'NameLink', 
-					'Bio', 
-					'Summary', 
-					'ImageDummy', 
-					'BodyContent', 
-					'Disclosure', 
-					'Position', 
-					'CreatedAt', 
-					'UpdatedAt', 
-					'BodyAll', 
-					'ArticleNumber', 
-					'ArticleUrl'
+	while row:
+		# Dataframe is better when outputing to csv. Naive f.write will not take good care of line breaks
+		"""
+		with open("result.csv", "w") as f:
+			f.write("Title, Date, Time, TickersAbout, TickersIncludes, Name, NameLink, Bio, Summary, ImageDummy, BodyContent, \
+				Disclosure, Position, CreatedAt, UpdatedAt, BodyAll, ArticleNumber, ArticleUrl")
+			f.write(',\n')
+			while row:
+				
+				f.write("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17}".
+				format(row['Title'].encode("utf-8"), row['Date'], row['Time'], row['TickersAbout'], row['TickersIncludes'], row['Name'].encode("utf-8"), 
+					row['NameLink'].encode("utf-8"), row['Bio'].encode("utf-8"), row['Summary'].encode("utf-8"), row['ImageDummy'], row['BodyContent'].encode("utf-8"), row['Disclosure'].encode("utf-8")
+					, row['Position'], row['CreatedAt'], row['UpdatedAt'], row['BodyAll'].encode("utf-8"), row['ArticleNumber'], row['ArticleUrl'].encode("utf-8"))) 
+				f.write(',\n')
+		"""
+		df = df.append({'Title':row['Title'], 
+					'Date': row['Date'], 
+					'Time': row['Time'], 
+					'TickersAbout': row['TickersAbout'], 
+					'TickersIncludes': row['TickersIncludes'], 
+					'Name': row['Name'], 
+					'NameLink': row['NameLink'], 
+					'Bio': row['Bio'], 
+					'Summary': row['Summary'], 
+					'ImageDummy': row['ImageDummy'], 
+					'BodyContent': row['BodyContent'], 
+					'Disclosure': row['Disclosure'], 
+					'Position': row['Position'], 
+					'CreatedAt': row['CreatedAt'], 
+					'UpdatedAt': row['UpdatedAt'], 
+					'BodyAll': row['BodyAll'], 
+					'ArticleNumber': row['ArticleNumber'], 
+					'ArticleUrl': row['ArticleUrl']
 					},ignore_index=True)
-"""
+		row = cursor_para.fetchone()
+
+	df.to_csv('result.csv', encoding = 'utf-8')
+	
+
 	
 if __name__ == '__main__':
 	cur = MSSQL_Connect()
